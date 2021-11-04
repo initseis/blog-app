51147373b7567b87968fffeda5f115c6705d9d3b
Kevin <kevinsevinche@gmail.com>
2021-11-04 16:01:57 -0500

    Update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,15 @@
 class LikesController < ApplicationController
-  def like
+  def create
     @like = Like.new
     @like.author_id = ApplicationController.current_user.id
     @like.post_id = params[:id]
+    @post = Post.find(params[:id])
     if @like.save
+      Like.update_likes_counter(@post)
+      flash[:notice] = 'Liked'
       redirect_to user_post_url(id: params[:id], user_id: params[:user_id])
     else
-      flash.now[:error] = 'To-do item update failed'
+      flash[:error] = 'Like Failed'
       redirect_to user_posts_url(user_id: params[:user_id])
     end
   end
