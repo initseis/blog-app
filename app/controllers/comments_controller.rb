e3430251807f04863c8e9307cb9f9fdea6abf45d
Kevin <kevinsevinche@gmail.com>
2021-11-04 16:01:45 -0500

    Update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,10 @@ class CommentsController < ApplicationController
     @comment.author_id = ApplicationController.current_user.id
     @comment.post_id = params[:id]
     if @comment.save
+      flash[:notice] = 'Comment added'
       redirect_to user_post_url(id: params[:id], user_id: params[:user_id])
     else
+      flash[:notice] = 'Comment not added'
       render :new
     end
   end
