0096ef0b4d5e22817a0f5f626405059673829e37
Kevin <kevinsevinche@gmail.com>
2021-11-08 18:21:56 -0500

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, foreign_key: 'author_id', class_name: 'Post'
-  has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
-  has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+  has_many :posts, foreign_key: 'author_id', class_name: 'Post', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', class_name: 'Comment', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', class_name: 'Like', dependent: :destroy
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
