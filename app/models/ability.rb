d3a5466970f7cbbfbb6b6a763242718ff8b276ad
Kevin <kevinsevinche@gmail.com>
2021-11-09 15:30:48 -0500

    Update ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,10 +6,11 @@ class Ability
   def initialize(user)
     can :read, Post
     can :create, Comment
-    can :manage, Post, author_id: user.id 
-    can :manage, Comment, author_id: user.id 
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
     return unless user.admin?
-    can :manage, Post 
-    can :manage, Comment 
+
+    can :manage, Post
+    can :manage, Comment
   end
 end
