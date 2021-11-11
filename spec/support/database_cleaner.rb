95e806bf59ec6196083fb641ce8091332d07e38b
Kevin <kevinsevinche@gmail.com>
2021-11-10 21:53:17 -0500

    Setup database cleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,17 +1,22 @@
 RSpec.configure do |config|
+  config.use_transactional_fixtures = false
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w[ar_internal_metadata]
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
   config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.after(:all) do
     DatabaseCleaner.clean
   end
 end
